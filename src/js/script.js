e16d72f52c906d0f4b2f717dc67f54405143c148
Nikolay <niko2892@gmail.com>
2020-09-19 16:38:22 +0300

    project v1

--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,83 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const refill = document.querySelector('.menu__rect_blue'),
-		update = document.querySelector('.menu__rect_yellow'),
-		mainSection = document.querySelector('.main'),
-		introSection = document.querySelector('.intro'),
-		abilSection = document.querySelector('.abil'),
-		refillSection = document.querySelector('.refill'),
-		updateSection = document.querySelector('.update'),
-		footer = document.querySelector('.footer');
+			const sectionLogo = document.querySelectorAll('.section__logo'),
+				  refillBtn = document.querySelectorAll('.menu__rect_blue'),
+				  updateBtn = document.querySelectorAll('.menu__rect_yellow'),
+				  repairBtn = document.querySelectorAll('.menu__rect_pink'),
+				  tradeBtn = document.querySelectorAll('.menu__rect_black'),
+				  mainSection = document.querySelector('.main'),
+				  introSection = document.querySelector('.intro'),
+				  abilSection = document.querySelector('.abil'),
+				  refillSection = document.querySelector('.refill'),
+				  updateSection = document.querySelector('.update'),
+				  repairSection = document.querySelector('.repair'),
+				  tradeSection = document.querySelector('.trade');
 
-	function fadeSection(section) {
-		section.style.display = 'none';
-	}
+			function hideSection(section) {
+				section.style.display = 'none';
+			} 
 
-	function showSection(section) {
-		section.style.display = "block";
-	}
+			function showSection(section) {
+				section.style.display = 'block';
+			}
 
+			sectionLogo.forEach(btn => {
+				btn.addEventListener('click', () => {
+					hideSection(refillSection);
+					hideSection(updateSection);
+					hideSection(repairSection);
+					hideSection(tradeSection);
+					showSection(mainSection);
+					showSection(introSection);
+					showSection(abilSection);
+				});
+			});
+			
+			refillBtn.forEach(btn => {
+				btn.addEventListener('click', () => {
+					hideSection(mainSection);
+					hideSection(introSection);
+					hideSection(abilSection);
+					hideSection(updateSection);
+					hideSection(repairSection);
+					hideSection(tradeSection);
+					showSection(refillSection);
+					
+				});
+			});
 
-	refill.addEventListener('click', () => {
-		mainSection.classList.add('main_animated');
-		introSection.classList.add('intro_animated');
-		abilSection.classList.add('abil_animated');
-		refillSection.classList.add('refill_animated');
-		footer.classList.add('footer_animated');
-		setTimeout(showSection, 800, refillSection);
-		setTimeout(fadeSection, 800, mainSection);
-		setTimeout(fadeSection, 800, introSection);
-		setTimeout(fadeSection, 800, abilSection);
-	});
+			updateBtn.forEach(btn => {
+				btn.addEventListener('click', () => {
+					hideSection(mainSection);
+					hideSection(introSection);
+					hideSection(abilSection);
+					hideSection(refillSection);
+					hideSection(repairSection);
+					hideSection(tradeSection);
+					showSection(updateSection);
+				});
+			});
 
-	
-	update.addEventListener('click', () => {
-		mainSection.classList.add('main_animated');
-		introSection.classList.add('intro_animated');
-		abilSection.classList.add('abil_animated');
-		updateSection.classList.add('refill_animated');
-		footer.classList.add('footer_animated');
-		setTimeout(showSection, 800, updateSection);
-		setTimeout(fadeSection, 800, mainSection);
-		setTimeout(fadeSection, 800, introSection);
-		setTimeout(fadeSection, 800, abilSection);
-	});
-});
+			repairBtn.forEach(btn => {
+				btn.addEventListener('click', () => {
+					hideSection(mainSection);
+					hideSection(introSection);
+					hideSection(abilSection);
+					hideSection(updateSection);
+					hideSection(refillSection);
+					hideSection(tradeSection);
+					showSection(repairSection);
+				});
+			});
+
+			tradeBtn.forEach(btn => {
+				btn.addEventListener('click', () => {
+					hideSection(mainSection);
+					hideSection(introSection);
+					hideSection(abilSection);
+					hideSection(updateSection);
+					hideSection(repairSection);
+					hideSection(refillSection);
+					showSection(tradeSection);
+				});
+			});
+		});
